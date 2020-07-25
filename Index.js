694fdaeffa48a9bf92261290d9a1a2782838e8c2
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2020-07-25 17:39:29 +0200

    Fixed token not being used from config, changed formatting and other minor changes.

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js")
-const tokenpath = require("../token.json")
 const config = require('./config.json')
 const fs = require('fs')
 
@@ -12,39 +11,47 @@ bot.on("ready", async function() {
     if(err)console.log('error: '+ err )
     loadedSounds = files.filter(f => f.split('.').pop() === 'mp3')
     loadedSounds.forEach((e, i) => {
-      loadedSounds[i] = e.replace(".mp3", "");
-    })
+      loadedSounds[i] = e.replace(".mp3", "") })
+
+    module.exports.loadedSounds = loadedSounds
     console.log(loadedSounds.length + ' Sound(s) loaded.')
    })
+
+   if (config.usewebserver) require("./webserver.js") //Webserver is enabled? Run it.
 })
+
 function LoadLogin() {
-  if (config.useToken === 'false') bot.login(tokenpath.token); 
-  else clientInformation.login(config.token);
+  if (config.useToken == false) bot.login(require("../token.json").token); 
+    else bot.login(config.token);
 }
 
 bot.on('message', async function(message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(config.prefix)) return;
   var args = message.content.substring(config.prefix.length).split(/\s+/);
-    switch(args[0].toLowerCase()) {
-      
+
+  switch(args[0].toLowerCase()) {
   case 'ping':
     message.channel.send('Pong.');
     break;
 
   case 'stop':
     message.channel.send('Goodbye!').then(m => {
-    bot.destroy()});
+    bot.destroy() });
     break;
 
   case 'restart':
     message.channel.send('Restarting...');
     bot.destroy();
-    LoadLogin();
+    setTimeout(() => {
+      LoadLogin();
+    }, 1000);
     break;
+
   case 'help':
     message.channel.send('Bot-Commands:\n//ping\n//stop\n//restart (This Command doesn´t load new Content)\n//join\n//leave\n//sound (Type sound for more help))')
     break;
+
   case 'join':
     if (message.member.voice.channel == null) { //Check if the user is in a voice channel
       message.channel.send("Please join a voice channel first!")
@@ -52,6 +59,7 @@ bot.on('message', async function(message) {
     if(message.member.voice.channel == null) {
       message.channel.send('please join a Channel first!')
     return; }
+
     message.member.voice.channel.join();
     console.log(`Joined ${message.member.voice.channel.name}.`)
     message.channel.send("Joined `" + message.member.voice.channel.name + "`.")
@@ -63,16 +71,17 @@ bot.on('message', async function(message) {
     message.channel.send("Left `" + message.guild.voice.connection.channel.name + "`.")
     message.guild.voice.connection.disconnect()
     break;
-  case 'sound': 
-      //if(args[1] === 'add') {message.channel.send('Adding Sound to the Board')}
-      if(args[1] === 'list')   message.channel.send(loadedSounds) //else if
-      else if(args[1] === 'dplay') {message.channel.send('Playing Sound')
-                                    message.member.guild.voice.connection.play(message.attachments.array()[0]["attachment"])}
-      else if(args[1] === 'play')  {message.channel.send('Playing Sound: ' + args[2])   
-                                    message.member.guild.voice.connection.play('./Sounds/'+ args[2]+'.mp3')}
-      else message.channel.send('Please use //sound play [title], \nsend a mp3 with the Comment "//sound dplay" or \nadd a Sound to the Folder')
 
-   
+  case 'sound': 
+    //if(args[1] === 'add') {message.channel.send('Adding Sound to the Board')}
+    if(args[1] === 'list') message.channel.send(loadedSounds) //else if
+    else if(args[1] === 'dplay') {
+      message.channel.send('Playing Sound')
+      message.member.guild.voice.connection.play(message.attachments.array()[0]["attachment"]) }
+    else if(args[1] === 'play') {
+      message.channel.send('Playing Sound: ' + args[2])
+      message.member.guild.voice.connection.play('./Sounds/'+ args[2]+'.mp3') }
+    else message.channel.send('Please use //sound play [title], \nsend a mp3 with the Comment "//sound dplay" or \nadd a Sound to the Folder')
   }
 });
 
